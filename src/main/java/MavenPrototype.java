7769dac3abedb587ce8e9e4594edeeeaea80e6fe
planetachiles <67108795+planetachiles@users.noreply.github.com>
2024-01-31 17:33:08 +0000

    Initial commit

---
+++ src/main/java/MavenPrototype.java
@@ -0,0 +1,23 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+
+public class MavenPrototype
+  {
+    public static void main(String[] args)
+    {
+
+      MavenPrototype m=new MavenPrototype();
+      m.addition("welcome");
+      System.out.println("hello world");
+      
+    }
+public String addition(String test)
+    {
+return test;
+    }
+    
+
+  
+  }
